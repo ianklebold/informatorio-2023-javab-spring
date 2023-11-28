89d64b6bdcf0010f84b2cf1f5eb80289bb474d52
ianklebold <ianstgo@gmail.com>
2023-11-28 20:57:11 -0300

    first commit

---
+++ breakmarket/src/main/java/com/info/breakmarket/BreakmarketApplication.java
@@ -0,0 +1,13 @@
+package com.info.breakmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakmarketApplication.class, args);
+	}
+
+}
---
+++ breakmarket/src/test/java/com/info/breakmarket/BreakmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.breakmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
